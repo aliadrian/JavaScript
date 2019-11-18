30b92653b1d10dd55b9fa52c1a8e0442ec3edfbf
Ali Adrian <alinasrat95@gmail.com>
2019-11-18 13:45:07 +0100

    Lesson 1

---
+++ Lesson 1/console/script.js
@@ -0,0 +1,38 @@
+// Comments can be written like this
+/* and this */
+
+/* Markup Language          Script language         Programming Language
+   HTML                     JavaScript              C    
+   CSS                      PowerShell              C++
+   XML                      cmdLets                 C#
+                            Python                  Java
+                                                    Python
+
+
+    High Level Language (between user and system)
+    - - - - - - - - - - - - - - - - - - - - - - -
+    (JavaScript)
+    Python
+    C# / Java
+    Delphi
+
+
+    Low Level Language (between system and hardware)
+    - - - - - - - - - - - - - - - - - - - - - - -
+    C++
+    C
+    Cobol
+    Ansembler
+    Microcode
+*/
+
+// Outputs on the website
+document.body.innerText = 'This is a simple paragraph.';
+
+// Outputs in the console
+console.log('Text in the console');
+console.warn('This is a warning!');
+console.error('This is an error!');
+
+// SANDBOX - DEVELOPER - We log everything here!
+// PRODUCTION - REAL-LIFE SITUATION - We log very little here
