9536085f821a8a724eca0f1ed7a871a52fdc374a
Ali Adrian <alinasrat95@gmail.com>
2019-11-25 14:15:59 +0100

    Lesson 3

---
+++ Lesson 3/events/script.js
@@ -0,0 +1,94 @@
+$(function() {
+    // $('#btnAlert').click(() => {
+    //     alert('Warning!');
+    // });
+
+    // $('#btnAlert').on('click', function() {
+    //     alert('Warning');
+    // });
+
+    // $('#btnAlert').dblclick(() => {
+    //     alert('Warning');
+    // });
+
+    // $('#btnAlert')
+    // .mouseenter(() => {
+    //     alert('Warning');
+    // })
+    // .mouseleave(() => {
+    //     alert('Warning');
+    // });
+
+    // $('input')
+    // .mouseenter(function() {
+    //     $(this).css({
+    //         'background': '#ccc',
+    //         'border': '1px solid #999'
+    //     });
+    // })
+    // .mouseleave(function() {
+    //     $(this).css({
+    //             'background': 'white',
+    //             'border': '1px solid #999'
+    //         });
+    // })
+
+    // $('input')
+    // .focus(function() {
+    //     $(this).css('background', '#ccc');
+    // })
+    // .blur(function() {
+    //     $(this).css('background', 'white');
+    // })
+
+    // Vanilla Javacsript
+    // document.getElementById('firstName').addEventListener('click', changeColor);
+
+    // function changeColor() {
+    //     this.style.background = '#ccc';
+    // }
+
+
+    // var style = document.createElement('style');
+    // style.innerHTML = `
+    // #firstName {
+    //     background: #ccc;
+    // }
+    // `;
+    // document.input.appendChild(style);
+
+    // $('input').keyup(function(e) {
+    //     if(e.key === 'Enter' || e.keyCode === 13) {
+    //         console.log('You have pressed Enter');
+    //     }
+    // });
+
+    // $('input').keyup(function(event) {
+    //     console.log(event);
+    // });
+
+    // var name = 'Sofia';
+
+    // $('input').keyup(function() {
+    //     if(event.target.value === 'Sofia') {
+    //         console.log(`${name} är BÄST!`);
+    //     }
+    // });
+
+    // $('form').submit((e) => {
+    //     e.preventDefault();
+
+    //     console.log('Submit');
+    // });
+
+
+    // $('#loginForm').submit((e) => {
+
+    //     let = writeToDatabaseCompleted = false;
+
+    //     if(!(writeToDatabaseCompleted)) {
+    //         console.error('Something went wrong');
+    //         return false;
+    //     }
+    // });
+});
